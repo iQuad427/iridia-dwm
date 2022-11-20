de9a3abe58a77441579be6ed55279f88fd242d31
iQuad427 <quentin.roels@gmail.com>
2022-11-20 11:16:47 +0100

    Made the threads of CMSIS RTOS work in Keil ('thread' project)

---
+++ src/embedded/Kiel µVision/dwm-simple/Keil µVision/_build/main.d
@@ -0,0 +1,5 @@
+.\_build\main.o: ..\main.c
+.\_build\main.o: ..\include\dwm.h
+.\_build\main.o: Q:\Keil_v5\ARM\ARMCCv506u7\Bin\..\include\stdbool.h
+.\_build\main.o: Q:\Keil_v5\ARM\ARMCCv506u7\Bin\..\include\stdint.h
+.\_build\main.o: Q:\Keil_v5\ARM\ARMCCv506u7\Bin\..\include\stdio.h
